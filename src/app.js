f44673d82d27141ef49676ea4fbf9ef509b66357
umaedi <umaedi.kh.99@gmail.com>
2025-05-13 00:21:40 +0700

    fix: reorder imports in app.js for consistency

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import { web } from "./application/web.js";
-import { errorMiddleware } from "./middleware/error-middleware.js";
 import dotenv from "dotenv";
 dotenv.config();
+import { web } from "./application/web.js";
+import { errorMiddleware } from "./middleware/error-middleware.js";
 const PORT = process.env.PORT;
 web.listen(PORT, () => {
   console.log("Server started on http://localhost:" + PORT);
