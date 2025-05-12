9eba1441d1ec126af1dcb3ff3de5115b1f3e78b7
umaedi <umaedi.kh.99@gmail.com>
2025-05-12 23:20:37 +0700

    refactor: switch from ES module to CommonJS syntax in public API router

--- src/router/public-api.js
+++ src/router/public-api.js
@@ -1,5 +1,5 @@
-import express from "express";
-import healthController from "../controller/healthController.js";
+const express = require("express");
+const healthController = require("../controller/healthController");
 
 const publicRouter = new express.Router();
 publicRouter.get("/ping", healthController.ping);
