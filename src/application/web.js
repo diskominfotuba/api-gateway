b40480f95918aaab5a1c5d431087cbdb1a5ceb05
umaedi <umaedi.kh.99@gmail.com>
2025-05-13 00:17:33 +0700

    fix: move dotenv configuration to app.js for proper environment variable loading

--- src/application/web.js
+++ src/application/web.js
@@ -1,8 +1,6 @@
 import express from "express";
 import { publicRouter } from "../router/public-api.js";
 import proxyRequest from "../services/serviceProxy.js";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const web = express();
 web.use(proxyRequest);
