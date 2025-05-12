1a1ccf81174f9068842267dbd00c43a2c637a44a
umaedi <umaedi.kh.99@gmail.com>
2025-05-12 22:13:21 +0700

    feat: add ecosystem configuration for api-gateway

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+// ecosystem.config.js
+export default {
+  apps: [
+    {
+      name: "api-gateway",
+      script: "./src/app.js",
+      exec_mode: "fork",
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+        INSTAGRAM_URL: "http://192.168.11.2:9913",
+        NOTIFICATION_SERVICE_URL: "http://localhost:3003",
+        PING_SERVICE: "http://localhost:3004",
+        // Tambahkan semua ENV dari file .env kamu di sini
+      },
+    },
+  ],
+};
