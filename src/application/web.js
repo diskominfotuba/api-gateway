7ffce00f8840fb31a14c06f0ecad458626a085a6
umaedi <umaedi.kh.99@gmail.com>
2025-05-12 19:43:24 +0700

    fix: uncomment proxyRequest import and middleware usage in web application

--- src/application/web.js
+++ src/application/web.js
@@ -1,15 +1,11 @@
 import express from "express";
 import { publicRouter } from "../router/public-api.js";
-// import proxyRequest from "../services/serviceProxy.js";
+import proxyRequest from "../services/serviceProxy.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const web = express();
-
-web.get("/ping", (req, res) => {
-  res.status(200).send("pong");
-});
+web.use(proxyRequest);
+web.use(express.json());
 
 web.use(publicRouter);
-// web.use(proxyRequest);
-web.use(express.json());
