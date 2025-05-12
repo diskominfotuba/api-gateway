7c0b941a941f2e828ad0346f1e4b2b84e8bd19cc
umaedi <umaedi.kh.99@gmail.com>
2025-05-12 19:39:45 +0700

    fix: comment out proxyRequest import and usage in web application

--- src/application/web.js
+++ src/application/web.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { publicRouter } from "../router/public-api.js";
-import proxyRequest from "../services/serviceProxy.js";
+// import proxyRequest from "../services/serviceProxy.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -11,5 +11,5 @@ web.get("/ping", (req, res) => {
 });
 
 web.use(publicRouter);
-web.use(proxyRequest);
+// web.use(proxyRequest);
 web.use(express.json());
