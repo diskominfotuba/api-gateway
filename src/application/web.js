45a1be44baf14fd009cac75fa8f437942880ef0f
umaedi <umaedi.kh.99@gmail.com>
2025-05-12 23:44:07 +0700

    fix: properly load environment variables by using dotenv configuration in web middleware

--- src/application/web.js
+++ src/application/web.js
@@ -2,9 +2,10 @@ import express from "express";
 import { publicRouter } from "../router/public-api.js";
 import proxyRequest from "../services/serviceProxy.js";
 import dotenv from "dotenv";
-dotenv.config();
+// dotenv.config();
 
 export const web = express();
+web.use(dotenv.config());
 web.use(proxyRequest);
 web.use(express.json());
 
